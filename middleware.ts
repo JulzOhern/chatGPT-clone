0691c0baa21c1e307d1b6c663aebffaf7785e5ac
julz <owanojulius742002@gmail.com>
2024-08-27 17:22:40 +0800

    Fix again the publick route for clerk webhook

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
-  "/api/clerk/webhooks/(.*)",
+  "/api(.*)",
 ]);
 
 export default clerkMiddleware((auth, request) => {
